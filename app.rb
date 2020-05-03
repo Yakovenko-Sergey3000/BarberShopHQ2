a03016f92a0c85d5d684b334712c61fcf4be1c51
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-05-03 20:10:32 +0500

    Add bookings page

--- app.rb
+++ app.rb
@@ -67,3 +67,12 @@ get '/barber/:id' do
     @barber = Barber.find(params[:id])
     erb :barber
 end   
+
+get '/bookings' do 
+    @clients = Client.order('created_at DESC')
+    erb :bookings
+end   
+
+get '/client/:id' do 
+  erb "hello"
+end   
